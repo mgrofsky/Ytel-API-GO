e14d5a9c94efe1836d404b2afbe8bcc150ce4bed
mgrofsky <git@apimatic.io>
2016-12-12 21:33:05 +0000

    Changed WebRTC output to JSON String

--- src/message360_lib/webrtc_pkg/Client.go
+++ src/message360_lib/webrtc_pkg/Client.go
@@ -1,7 +1,7 @@
 /*
  * message360_lib
  *
- * This file was automatically generated for message360 by APIMATIC v2.0 ( https://apimatic.io ) on 12/08/2016
+ * This file was automatically generated for message360 by APIMATIC v2.0 ( https://apimatic.io ) on 12/12/2016
  */
 package webrtc_pkg
 
@@ -11,14 +11,6 @@ import(
 	"message360_lib"
 	"message360_lib/apihelper_pkg"
 )
-
-/*
- * Input structure for the method CreateToken
- */
-type CreateTokenInput struct {
-    AccountSid      string          //Your message360 Account SID
-    AuthToken       string          //Your message360 Token
-}
 
 /*
  * Input structure for the method CreateCheckFunds
@@ -38,73 +30,24 @@ type CreateAuthenticateNumberInput struct {
 }
 
 /*
+ * Input structure for the method CreateToken
+ */
+type CreateTokenInput struct {
+    AccountSid      string          //Your message360 Account SID
+    AuthToken       string          //Your message360 Token
+}
+
+/*
  * Client structure as interface implementation
  */
 type WEBRTC_IMPL struct { }
 
 /**
- * message360 webrtc
- * @param  CreateTokenInput     Structure with all inputs
- * @return	Returns the  response from the API call
- */
-func (me *WEBRTC_IMPL) CreateToken (input *CreateTokenInput) (error) {
-        //the base uri for api requests
-    _queryBuilder := message360_lib.BASEURI;
-
-    //prepare query string for API call
-   _queryBuilder = _queryBuilder + "/webrtc/createToken.json"
-
-    //variable to hold errors
-    var err error = nil
-    //validate and preprocess url
-    _queryBuilder, err = apihelper_pkg.CleanUrl(_queryBuilder)
-    if err != nil {
-        //error in url validation or cleaning
-        return err
-    }
-
-    //prepare headers for the outgoing request
-    headers := map[string]interface{} {
-        "user-agent" : "message360-api",
-    }
-
-    //form parameters
-    parameters := map[string]interface{} {
-
-        "account_sid" : input.AccountSid,
-        "auth_token" : input.AuthToken,
-
-    }
-
-
-    //prepare API request
-    _request := unirest.PostWithAuth(_queryBuilder, headers, parameters, message360_lib.Config.BasicAuthUserName, message360_lib.Config.BasicAuthPassword)
-    //and invoke the API call request to fetch the response
-    _response, err := unirest.AsString(_request);
-    if err != nil {
-        //error in API invocation
-        return err
-    }
-
-    //error handling using HTTP status codes
-    if (_response.Code < 200) || (_response.Code > 206) { //[200,206] = HTTP OK
-        err = apihelper_pkg.NewAPIError("HTTP Response Not OK" , _response.Code, _response.RawBody)
-    }
-    if(err != nil) {
-        //error detected in status code validation
-        return err
-    }
-
-    //returning the response
-    return nil
-}
-
-/**
  * TODO: type endpoint description here
  * @param  CreateCheckFundsInput     Structure with all inputs
- * @return	Returns the  response from the API call
+ * @return	Returns the string response from the API call
  */
-func (me *WEBRTC_IMPL) CreateCheckFunds (input *CreateCheckFundsInput) (error) {
+func (me *WEBRTC_IMPL) CreateCheckFunds (input *CreateCheckFundsInput) (string, error) {
         //the base uri for api requests
     _queryBuilder := message360_lib.BASEURI;
 
@@ -117,7 +60,7 @@ func (me *WEBRTC_IMPL) CreateCheckFunds (input *CreateCheckFundsInput) (error) {
     _queryBuilder, err = apihelper_pkg.CleanUrl(_queryBuilder)
     if err != nil {
         //error in url validation or cleaning
-        return err
+        return "", err
     }
 
     //prepare headers for the outgoing request
@@ -140,7 +83,7 @@ func (me *WEBRTC_IMPL) CreateCheckFunds (input *CreateCheckFundsInput) (error) {
     _response, err := unirest.AsString(_request);
     if err != nil {
         //error in API invocation
-        return err
+        return "", err
     }
 
     //error handling using HTTP status codes
@@ -149,19 +92,19 @@ func (me *WEBRTC_IMPL) CreateCheckFunds (input *CreateCheckFundsInput) (error) {
     }
     if(err != nil) {
         //error detected in status code validation
-        return err
+        return "", err
     }
 
     //returning the response
-    return nil
+    return _response.Body, nil
 }
 
 /**
  * Authenticate a message360 number for use
  * @param  CreateAuthenticateNumberInput     Structure with all inputs
- * @return	Returns the  response from the API call
+ * @return	Returns the string response from the API call
  */
-func (me *WEBRTC_IMPL) CreateAuthenticateNumber (input *CreateAuthenticateNumberInput) (error) {
+func (me *WEBRTC_IMPL) CreateAuthenticateNumber (input *CreateAuthenticateNumberInput) (string, error) {
         //the base uri for api requests
     _queryBuilder := message360_lib.BASEURI;
 
@@ -174,7 +117,7 @@ func (me *WEBRTC_IMPL) CreateAuthenticateNumber (input *CreateAuthenticateNumber
     _queryBuilder, err = apihelper_pkg.CleanUrl(_queryBuilder)
     if err != nil {
         //error in url validation or cleaning
-        return err
+        return "", err
     }
 
     //prepare headers for the outgoing request
@@ -198,7 +141,7 @@ func (me *WEBRTC_IMPL) CreateAuthenticateNumber (input *CreateAuthenticateNumber
     _response, err := unirest.AsString(_request);
     if err != nil {
         //error in API invocation
-        return err
+        return "", err
     }
 
     //error handling using HTTP status codes
@@ -207,10 +150,67 @@ func (me *WEBRTC_IMPL) CreateAuthenticateNumber (input *CreateAuthenticateNumber
     }
     if(err != nil) {
         //error detected in status code validation
-        return err
+        return "", err
     }
 
     //returning the response
-    return nil
+    return _response.Body, nil
+}
+
+/**
+ * message360 webrtc
+ * @param  CreateTokenInput     Structure with all inputs
+ * @return	Returns the string response from the API call
+ */
+func (me *WEBRTC_IMPL) CreateToken (input *CreateTokenInput) (string, error) {
+        //the base uri for api requests
+    _queryBuilder := message360_lib.BASEURI;
+
+    //prepare query string for API call
+   _queryBuilder = _queryBuilder + "/webrtc/createToken.json"
+
+    //variable to hold errors
+    var err error = nil
+    //validate and preprocess url
+    _queryBuilder, err = apihelper_pkg.CleanUrl(_queryBuilder)
+    if err != nil {
+        //error in url validation or cleaning
+        return "", err
+    }
+
+    //prepare headers for the outgoing request
+    headers := map[string]interface{} {
+        "user-agent" : "message360-api",
+    }
+
+    //form parameters
+    parameters := map[string]interface{} {
+
+        "account_sid" : input.AccountSid,
+        "auth_token" : input.AuthToken,
+
+    }
+
+
+    //prepare API request
+    _request := unirest.PostWithAuth(_queryBuilder, headers, parameters, message360_lib.Config.BasicAuthUserName, message360_lib.Config.BasicAuthPassword)
+    //and invoke the API call request to fetch the response
+    _response, err := unirest.AsString(_request);
+    if err != nil {
+        //error in API invocation
+        return "", err
+    }
+
+    //error handling using HTTP status codes
+    if (_response.Code < 200) || (_response.Code > 206) { //[200,206] = HTTP OK
+        err = apihelper_pkg.NewAPIError("HTTP Response Not OK" , _response.Code, _response.RawBody)
+    }
+    if(err != nil) {
+        //error detected in status code validation
+        return "", err
+    }
+
+    //returning the response
+    return _response.Body, nil
 }
 
